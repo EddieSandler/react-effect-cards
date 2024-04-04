321fe634fa597f5ba36894ab6db2f98c12ef2ec0
Eddie Sandler <ed.sandler@gmail.com>
2024-04-03 20:11:24 -0400

    added shuffle and error handling. needs styling

--- src/Cards.js
+++ src/Cards.js
@@ -19,22 +19,41 @@ function Cards() {
   // Function to draw a card using the deckId
   const drawCard = async () => {
     if (deckId) {
-      const drawCardUrl = `${BASE_URL}/${deckId}/draw/?count=1`;
-      // Assuming you want to do something with the response
-      const res = await axios.get(drawCardUrl);
-      console.log(res.data); // Log or set state with the drawn card data
-    } else {
-      console.log('Game Over');
-    }
+      try {
+        const drawCardUrl = `${BASE_URL}/${deckId}/draw/?count=1`;
+        // Assuming you want to do something with the response
+        const res = await axios.get(drawCardUrl);
+        console.log(res.data.deck_id);
+        console.log(`${res.data.cards[0].value} of  ${res.data.cards[0].suit}`); // Log or set state with the drawn card data
+        let img = res.data.cards[0].image;
+        console.log(img);
+      }
+      catch {
+        console.log('Game Over');
+      }
+    };
   };
+
+  const shuffleDeck = async () => {
+    //remove all cards from screen
+    //call api to shuffle deck with existing deckId
+
+    const shuffleUrl = `${BASE_URL}/${deckId}/shuffle`;
+    // Assuming you want to do something with the response
+    const res = await axios.get(shuffleUrl);
+    console.log(res.data.deck_id); // Log or set state with the drawn card data
+  };
+  //button can't be clickable during shuffle
+
 
   return (
     <div>
-      <div>Test</div>
+      <div>Draw Cards</div>
       {/* Ensure drawCard is called without passing event arguments */}
       <button onClick={drawCard}>Draw Card</button>
+      <button onClick={shuffleDeck}>Shuffle Deck</button>
     </div>
   );
-}
+};
 
 export default Cards;
